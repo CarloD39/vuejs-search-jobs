f4f42076cbe056e7f57288e794a82f1d7b350ac1
CarloD39 <carlodurand@hotmail.it>
2021-07-06 17:27:56 +0200

    1

---
+++ js/script.js
@@ -0,0 +1,58 @@
+Vue.config.devtools = true;
+
+new Vue (
+    {
+        el: "#search-app",
+
+        data: {
+
+            jobs: [
+                {
+                  id: 1,
+                  company: 'Perferendis',
+                  position: 'Developer',
+                  description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+                  created_at: '5/22/2021',
+                  logo: 'logo.jpg',
+                  city: 'Roma',
+                  contract: 'Full Time'
+                },
+                {
+                  id: 2,
+                  company: 'johnson',
+                  position: 'Developer',
+                  description: 'Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+                  created_at: '10/22/2021',
+                  logo: 'logo.jpg',
+                  city: 'Roma',
+                  contract: 'Full Time'
+                },
+                {
+                  id: 3,
+                  company: 'Astra',
+                  position: 'Developer',
+                  description: 'Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+                  created_at: '02/22/2021',
+                  logo: 'logo.jpg',
+                  city: 'Roma',
+                  contract: 'Part Time'
+                }
+
+            ],
+
+            starred: [1, 2, 3],
+            applied: [4, 5]
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+)
